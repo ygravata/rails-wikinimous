3f9427da8c0a71abccc25bab0411b662048e5e39
Yuri Gravata <ygravata@gmail.com>
2020-04-21 19:30:35 -0300

    doing 3 later

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+10.times do
+  article = Article.new(
+    title: Faker::Books::CultureSeries.book,
+    content: Faker::Books::CultureSeries.culture_ship)
+  article.save!
+end
